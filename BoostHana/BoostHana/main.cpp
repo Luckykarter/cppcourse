9d6fa3d495df598a19dd1f85f2ae6d49ae7e9522
Egor Wexler <ferotex7@gmail.com>
2020-06-03 16:55:41 +0300

    desktop github
    test desktop

---
+++ BoostHana/BoostHana/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  BoostHana
+//
+//  Created by Egor Wexler on 03/06/2020.
+//  Copyright © 2020 Egor Wexler. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+int main(int argc, const char * argv[]) {
+    
+    fibonacci(15);
+}
